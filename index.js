68c05ec1b35856e646a04a739bd04d1793d38fff
natanaelcloud <natanpaivacosta@gmail.com>
2024-09-30 21:33:46 -0300

    adicionar JS em branco

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-
-const tempLondon = [18, 13, 8, 2]
-
-const tempPositive = tempLondon.every(function(value ){
-    return value >= 0
-})
-//every chega todos valores da array
-
-console.log(tempPositive)
+.
 
